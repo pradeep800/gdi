304eadf7647d307a9c4cde202ff0ff8521576994
Pradeep Singh <pradeep8b0@gmail.com>
2025-01-31 13:47:55 +0530

    remove bug on intersection

--- dataset/dataset.js
+++ dataset/dataset.js
@@ -12,45 +12,33 @@ module.exports = function(RED) {
 		node.name = config.name;
 
 		// Handle incoming messages
-		node.on('input', function(msg) {
-			// Use the dynamically configured values from the frontend
-			const name = node.name
+		// Use the dynamically configured values from the frontend
+		const name = node.name
 
-			if (!name) {
-				node.status({ fill: "red", shape: "dot", text: `no name` });
-				return;
-			}
-			if (!isValidString(name)) {
-				node.status({ fill: "red", shape: "dot", text: `node name only contain a-z, A-Z, 0-9, - and _` });
-				return;
-			}
-			if (!Array.isArray(msg.payload)) {
-				msg.payload = []
-			}
-			let n = msg.payload.length;
-			for (let i = 0; i < n; i++) {
-				if (name === msg.payload[i].name) {
-					node.status({ fill: "red", shape: "dot", text: `node with this name already present` });
-					return;
-				}
-
-			}
-
-			let depends = []
-			if (n !== 0) {
-				depends.push(msg.payload[n - 1].name)
-			}
-			msg.payload.push({
-				type: "dataset",
-				name: name,
-				depends,
-				args: []
-			})
+		if (!name) {
+			node.status({ fill: "red", shape: "dot", text: `no name` });
+			return;
+		}
+		if (!isValidString(name)) {
+			node.status({ fill: "red", shape: "dot", text: `node name only contain a-z, A-Z, 0-9, - and _` });
+			return;
+		}
+		let msg = {
+			payload: []
+		}
+		let depends = []
+		msg.payload.push({
+			type: "dataset",
+			name: name,
+			depends,
+			args: []
+		})
+		// nodes are taking time to registry there event so waiting for 1s to send event
+		setTimeout(() => {
 			node.send(msg);
-			node.status({ fill: "green", shape: "dot", text: `successfully published` });
-		});
+		}, 1000)
+		node.status({ fill: "green", shape: "dot", text: `successfully published` });
 	}
-
 	// Register the node with Node-RED
 	RED.nodes.registerType("dataset", GdiNode);
 };
